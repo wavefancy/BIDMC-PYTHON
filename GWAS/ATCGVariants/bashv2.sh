c550ee7efdafb7023e17d96e03e64b329008faa1
wavefancy <wavefancy@gmail.com>
2016-05-19 12:10:00 -0400

    check ac|tc variants sites.

---
+++ GWAS/ATCGVariants/bashv2.sh
@@ -0,0 +1,7 @@
+
+cat test.txt | python3 ATCGVariantsV2.py -a 5 -b 6
+
+#1       1:82154 0       82154   T       A
+#1       1:752566        0       752566  C       G
+#1       1:776546        0       776546  G       C
+#1       1:800007        0       800007  A       T
