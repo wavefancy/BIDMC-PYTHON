70bbf123c9d075c566b78fd66ccfa0550a2f3717
wavefancy <wavefancy@gmail.com>
2016-06-11 12:38:38 -0400

    distri plot UBD

--- PLOT/DistributionPlot/plotpdf.sh
+++ PLOT/DistributionPlot/plotpdf.sh
@@ -1,4 +1,4 @@
 
-cat test.txt | python3 DistributionPlotUBD.py -x myx -o temp-plot.html\
+cat test.txt | python3 DistributionPlotUBD2.py -x myx -o temp-plot.html -t\
 && phantomjs ~/scripts/js/rasterize.js temp-plot.html test.pdf 10in*5in \
 && display test.pdf
