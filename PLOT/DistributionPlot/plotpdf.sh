dfd2269bff7d1cb44441640f5ebccc648e23014e
wavefancy <wavefancy@gmail.com>
2016-06-15 09:53:59 -0400

    outliers plots

--- PLOT/DistributionPlot/plotpdf.sh
+++ PLOT/DistributionPlot/plotpdf.sh
@@ -1,4 +1,4 @@
 
-cat test.txt | python3 DistributionPlotUBD.py -x myx -o temp-plot.html\
+cat test.txt | python3 DisPlotESRDChr1-2.py -x myx -o temp-plot.html\
 && phantomjs ~/scripts/js/rasterize.js temp-plot.html test.pdf 10in*5in \
 && display test.pdf
