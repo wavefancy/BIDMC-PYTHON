e45e9c7535322e59bf9483ad0a41b728111ba12b
wavefancy <wavefancy@gmail.com>
2016-10-14 14:09:35 -0400

    update to version 2.0, fix error in version 1.0.

--- Exome/EstimateExpectedSuccessRate/EstimateExpectedSuccessRate.py
+++ Exome/EstimateExpectedSuccessRate/EstimateExpectedSuccessRate.py
@@ -7,7 +7,7 @@
     @Author: wavefancy@gmail.com
 
     Usage:
-        EstimateExpectedSuccessRate.py -t totalEvents
+        EstimateExpectedSuccessRate.py -b bgCount -s sCount (-a|-e)
         EstimateExpectedSuccessRate.py -h | --help | -v | --version | -f | --format
 
     Notes:
@@ -15,7 +15,11 @@
         3. Output results to stdout.
 
     Options:
-        -t int          Total number of events.
+        -b bgCount      Column index for background count(totoal numnber of trials).
+        -s sCount       Column index for the number of successful trails.
+        -a              Output average rate for each gene, totalSuccessTrails4AllGenes/totalTrails4AllGenes.
+        -e              Output empirical rate for each gene, totalSuccessTrails/totalTrails4Gene,
+                        *** If the successful rate is 0, replaced as average rate.
         -h --help       Show this screen.
         -v --version    Show version.
         -f --format     Show format example.
@@ -28,44 +32,65 @@ signal(SIGPIPE, SIG_DFL)
 def ShowFormat():
     '''Input File format example:'''
     print('''
-    input example(-t 100):
+    input example:
 ----------------------
-x1      3
-x2      5
-x3      10
+x1      3   1
+x2      5   2
+x3      10  0
 
-#output
+#output: cat test.txt | python3 EstimateExpectedSuccessRate.py -b 2 -s 3 -a
 ----------------------
-x1      3       1.6667e-01
-x2      5       2.7778e-01
-x3      10      5.5556e-01
+1.6667e-01
+
+#output: cat test.txt | python3 EstimateExpectedSuccessRate.py -b 2 -s 3 -e
+----------------------
+x1      3       1       3.3333e-01
+x2      5       2       4.0000e-01
+x3      10      0       1.6667e-01
     ''');
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='1.0')
+    args = docopt(__doc__, version='2.0')
     #print(args)
 
     if(args['--format']):
         ShowFormat()
         sys.exit(-1)
 
-    totalEvents = args['-t']
+    bIndex = int(args['-b']) -1
+    sIndex = int(args['-s']) -1
 
-    oTotal = 0
     content = []
     for line in sys.stdin:
         line = line.strip()
         if line:
             ss = line.split()
-            x = int(ss[1])
-            content.append((ss[0],x))
-            oTotal += x
+            try:
+                b = int(ss[bIndex])
+                s = int(ss[sIndex])
 
-    #output
-    for x in content:
-        out = [x[0], str(x[1])]
-        out.append('%.4e'%(x[1]*1.0/oTotal))
-        sys.stdout.write('%s\n'%('\t'.join(out)))
+                ss.append(b) #background
+                ss.append(s) #success
+                content.append(ss)
+            except ValueError:
+                sys.stderr.write('Warning, parse int error at(SKIPPED): %s\n'%(line))
+
+    #compute average rate.
+    sTotal = sum([x[-1] for x in content])
+    bTotal = sum([x[-2] for x in content])
+
+    if args['-a']:
+        sys.stdout.write('%.4e\n'%(1.0*sTotal/bTotal))
+
+    if args['-e']:
+        for x in content:
+            out = x[:-2]
+            if x[-1] == 0:
+                out.append('%.4e'%(1.0*sTotal/bTotal))
+            else:
+                out.append('%.4e'%(x[-1]*1.0/x[-2]))
+
+            sys.stdout.write('%s\n'%('\t'.join(out)))
 
 sys.stdout.close()
 sys.stderr.flush()
