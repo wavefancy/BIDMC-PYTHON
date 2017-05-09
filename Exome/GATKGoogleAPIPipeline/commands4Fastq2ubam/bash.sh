959fd8ae77362989024bc8afd88760f6e58cb9c8
wallace-mac <wavefancy@gmail.com>
2017-05-09 00:49:07 -0400

    v1

---
+++ Exome/GATKGoogleAPIPipeline/commands4Fastq2ubam/bash.sh
@@ -0,0 +1,4 @@
+
+python3 commands4Fastq2ubam.py -l LIB -j ~/jars/picard.jar test_R1_001.txt
+
+#java -jar -Xmx8G /Users/wallace/jars/picard.jar FastqToSam FASTQ=/Users/wallace/Files/ProjectSource/BIDMC-PYTHON/Exome/GATKGoogleAPIPipeline/commands4Fastq2ubam/test_R1_001.txt FASTQ2=/Users/wallace/Files/ProjectSource/BIDMC-PYTHON/Exome/GATKGoogleAPIPipeline/commands4Fastq2ubam/test_R2_001.txt OUTPUT=data/test.bam READ_GROUP_NAME=HJCK2BBXX.3 SAMPLE_NAME=test LIBRARY_NAME=LIB PLATFORM_UNIT=47_HJCK2BBXX.3 PLATFORM=illumina SEQUENCING_CENTER=LIB
