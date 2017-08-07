ba0702534568402651044106e59a7c25480e3f04
wavefancy <wavefancy@gmail.com>
2017-08-07 17:08:15 -0400

    change to matplotlib for color scale

--- PLOT/Value2Color/Value2Color.py
+++ PLOT/Value2Color/Value2Color.py
@@ -6,8 +6,8 @@
     @Author: wavefancy@gmail.com
 
     Usage:
-        Value2Color.py -k int [--cmax int] [-r] [-n cname] [--rl float] [--rr float]
-        Value2Color.py -h | --help | -v | --version | -f | --format
+        Value2ColorV2.py -k int [-r] [-n cname] [--rl float] [--rr float]
+        Value2ColorV2.py -h | --help | -v | --version | -f | --format
 
     Notes:
         1. Read results from stdin, and output results to stdout.
@@ -15,15 +15,17 @@
 
     Options:
         -k int        Color index for input value.
-        --cmax int    Color scale max value, set all the input value larger than this as this value.
         --rl float    Set color range left end, default min(inputValues).
         --rr float    Set color range right end, default max(inputValues).
-        -r            Replace the value as color, otherwise append the color value at the line end.
-        -n cname      Color scale name, default YlOrRd, full list:
-                      https://plot.ly/ipython-notebooks/color-scales/
+        -r            Reverse color map.
+        -n cname      Color scale name, default Sequential.YlGnBu_4, full list:
+                      https://jiffyclub.github.io/palettable/colorbrewer/
         -h --help     Show this screen.
         -v --version  Show version.
         -f --format   Show input/output file format example.
+
+    API:
+        Palette names please ref. https://jiffyclub.github.io/palettable/#matplotlib-colormap
 """
 import sys
 from docopt import docopt
@@ -54,6 +56,7 @@ if __name__ == '__main__':
     col = int(args['-k']) -1
     indata = []
     zcolor = []
+    opacity = 1
     for line in sys.stdin:
         line = line.strip()
         if line:
@@ -67,47 +70,31 @@ if __name__ == '__main__':
             except ValueError:
                 sys.stdout.write('Warning: Parse Value Error (Skipped): %s\n'%(line))
 
-
-    #print(zcolor)
-    #color max
-    cmax = max(zcolor)
-    if args['--cmax']:
-        cmax = int(args['--cmax'])
-        zcolor = [ cmax if x >= cmax else x for x in zcolor]
-
-    import colorlover as cl
-    from numpy import interp
-    import webcolors
-    #https://plot.ly/ipython-notebooks/color-scales/
-    #define color map.
-    colorName = 'YlOrRd'
-    if args['-n']:
-        colorName = args['-n']
-    color = cl.scales['9']['seq'][colorName]
-
-    #map color to 500 bins, convert to rgb and then to numberic format of rgb.
-    c500 = cl.to_numeric(cl.to_rgb(cl.interp( color, 500 )))
-    #rgb to hex format.
-    c500 = [webcolors.rgb_to_hex((int(x[0]), int(x[1]), int(x[2]))) for x in c500]
+    #API ref: https://stackoverflow.com/questions/28752727/map-values-to-colors-in-matplotlib
+    #API ref: https://jiffyclub.github.io/palettable/colorbrewer/sequential/#previews
+    #API ref: https://matplotlib.org/api/cm_api.html
+    from palettable.colorbrewer.sequential import YlGnBu_4
+    import matplotlib
+    import matplotlib.cm as cm
+    #ax.imshow(data, cmap=Blues_8.mpl_colormap)
 
     vrange=[min(zcolor), max(zcolor)]
     if args['--rl']:
         vrange[0] = float(args['--rl'])
     if args['--rr']:
         vrange[1] = float(args['--rr'])
-    #value to color.
-    cindex = [interp(x, vrange, [0,499]) for x in zcolor]
-    oColor = [c500[int(x)] for x in cindex]
-    #oColor = [cl.to_numeric(c500[int(x)]) for x in cindex]
 
-    if args['-r']:
-        for x, y in zip(indata, oColor):
-            x[col] = y
-            sys.stdout.write('%s\n'%('\t'.join(x)))
-    else:
-        for x,y in zip(indata, oColor):
-            x.append(y)
-            sys.stdout.write('%s\n'%('\t'.join(x)))
+    norm = matplotlib.colors.Normalize(vmin=vrange[0], vmax=vrange[1], clip=True)
+    mapper = cm.ScalarMappable(norm=norm, cmap=YlGnBu_4.mpl_colormap)
+
+    # if args['-r']:
+    #     for x, y in zip(indata, oColor):
+    #         x[col] = y
+    #         sys.stdout.write('%s\n'%('\t'.join(x)))
+    # else:
+    for x,y in zip(indata, zcolor):
+        x.append(str(mapper.to_rgba(y,alpha=opacity,bytes=True,norm=True)).replace(' ',''))
+        sys.stdout.write('%s\n'%('\t'.join(x)))
 
 sys.stdout.flush()
 sys.stdout.close()
