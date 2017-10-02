98adb95277d1547b447005f150b667bdbefb3da2
wavefancy <wavefancy@gmail.com>
2017-10-02 13:06:55 -0400

    try merge

--- PLOT/BarPlot/plotpdf.sh
+++ PLOT/BarPlot/plotpdf.sh
@@ -1,3 +1,3 @@
-cat temp.txt | python3 BarPlot.py -y Frequency -o temp-plot.html --or 2 --cl '#FA1A1A,#0784FF,#8AC300'\
+cat temp.txt | python3 BarPlot.py -y Frequency -o temp-plot.html --or 2 --xdt 1 --gcl '#FA1A1A,#0784FF,#8AC300' --lloc 0 --ta '2_3_*' --ts 20 --bma 50 --rma 30\
 && phantomjs ~/scripts/js/rasterize.js temp-plot.html test.pdf 4in*4in \
 && display test.pdf
