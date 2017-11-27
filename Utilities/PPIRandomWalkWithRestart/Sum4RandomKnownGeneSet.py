a13002eeddd9ddf46d19850548fbd6a7cba49d5d
wavefancy <wavefancy@gmail.com>
2017-11-27 10:07:54 -0500

    dirty0.1

--- Utilities/PPIRandomWalkWithRestart/Sum4RandomKnownGeneSet.py
+++ Utilities/PPIRandomWalkWithRestart/Sum4RandomKnownGeneSet.py
@@ -54,55 +54,46 @@ if __name__ == '__main__':
     #version 2.0
     #1. add function for line comments.
 
-    commentDelimeter = '#'
-    linecomment = '//'
-    linebreaker = '!'
+    knownGeneSet = set()
+    sSize = -1 #random selection gene set size
+    rTimes = -1 #repeating times for the random selection.
 
-    if args['-d']:
-        commentDelimeter = args['-d']
-    if args['-l']:
-        linecomment = args['-l']
-    if args['-b']:
-        linebreaker = args['-b']
+    if args['-k']:
+        with open(args['-k'], mode='r') as kf:
+            for line in kf:
+                line = line.strip()
+                if line:
+                    knownGeneSet.add(line)
+
+    if args['-s']:
+        sSize = int(args['-s'])
+    if args['-r']:
+        rTimes = int(args['-r'])
     #print(args)
 
     if(args['--format']):
         ShowFormat()
         sys.exit(-1)
 
-    #split by lines.
-    import re
-    lines = args['<string>'].strip()
-    lines = lines.split(commentDelimeter)
-    if len(lines) %2 == 0:
-        sys.stderr.write('Error: Comment delimter should be appeared in pair!.\n')
-        sys.exit(-1)
+    allData = []
+    for line in sys.stdin:
+        line = line.strip()
+        if line:
+            ss = line.split()
+            allData.append((ss[0],float(ss[1])))
 
-    #skip comments
-    arr2 = []
-    for i in range(0,len(lines),2):
-        arr2.append(lines[i])
+    if knownGeneSet:
+        out = [x for x in allData if x[0] in knownGeneSet]
+        if len(out) != len(knownGeneSet):
+            sys.stderr.write('ERROR: Gene missing or repeat in input data!!!\n')
+            sys.exit(-1)
+        sys.stdout.write('%.4e\n'%(sum[x[1] for x in out]))
 
-    #remove line breakers.
-    arr1 = []
-    for x in arr2:
-        if x:
-            for y in x.splitlines():
-                y = y.strip()
-                #compatible with bash line breaker.
-                y = y.strip('\\')
-                if not y.startswith(linecomment): #skip line commented line.
-                    arr1.append(y)
-
-    out = ' '.join(arr1)
-    out = re.sub(r'\s+',' ',out)
-    out = out.split(linebreaker)
-    for x in out:
-        x = x.strip()
-        sys.stdout.write('%s\n'%(x))
-
-    # if len(lines) == 1:
-    #     lines = re.split('\\\\n',lines[0])
+    if rTimes >0:
+        import numpy.random as r
+        for i in range(rTimes):
+            tt = r.choice(allData, size=sSize, replace=False)
+            sys.stdout.write('%.4e\n'%(sum[x[1] for x in out]))
 
 sys.stdout.flush()
 sys.stdout.close()
